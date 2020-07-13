1ec6dd2d4d1ecba5af72f4a1c7996aae92104e8d
ELASTICPATH\dpendery <darren.pendery@elasticpath.com>
2020-07-13 14:52:41 +0100

    Added fulfillment items flow. Modified function to create fulfillment items with relationships to the fulfillment.

--- ordershipments/order-shipments/fulfillments.js
+++ ordershipments/order-shipments/fulfillments.js
@@ -7,9 +7,13 @@ const epccHeaders = require('./epccheaders');
 const orders = require('./orders');
 const fulfillmentContainers = require('./fulfillmentcontainers');
 
-const epCCFulfillmentssUrl = "https://api.moltin.com/v2/flows/fulfillments/entries";
+const epCCFulfillmentsUrl = "https://api.moltin.com/v2/flows/fulfillments/entries";
+const epCCFulfillmentItemsUrl = "https://api.moltin.com/v2/flows/fulfillmentItems/entries";
 const epCCFulFillmentContainers = "https://api.moltin.com/v2/flows/fulfillmentcontainers/entries";
 
+/**
+ * Main method for creating a fulfillment in EP CC.
+*/
 const postOrderFulfillment = async function (token, orderId, fulfillment) {
 
 	// GET order.
@@ -47,19 +51,67 @@ const postOrderFulfillment = async function (token, orderId, fulfillment) {
 	return newFulfillment;
 }
 
+/**
+ * Creates a fulfillment, its items and their relationships to the fulfillment.
+*/
 const postFulfillment = async function (token, fulfillment) {
 	var headers = epccHeaders.getHeaders('Bearer ' + token);
 
 	var newFulfillmentData = {
 		"data": {
 			"type": "entry",
-			"shipment_id": fulfillment.shipment_id,
-			"tracking_id": fulfillment.tracking_id,
-			"carrier": fulfillment.carrier
+			"fulfillmentId": fulfillment.id,
+			"adminNotes": fulfillment.adminNotes,
+			"aftershipId": fulfillment.aftershipId,
+			"channelAttempts": fulfillment.channelAttempts,
+			"channelError": fulfillment.channelError,
+			"commercialInvoiceUrl": fulfillment.commercialInvoiceUrl,
+			"createdAt": fulfillment.createdAt,
+			"days": fulfillment.days,
+			"durationTerms": fulfillment.durationTerms,
+			"externalId": fulfillment.externalId,
+			"files": fulfillment.files,
+			"fulfillmentType": fulfillment.fulfillmentType,
+			"grams": fulfillment.grams,
+			"intercomMessageId": fulfillment.intercomMessageId,
+			"inventoryStatus": fulfillment.inventoryStatus,
+			"labelError": fulfillment.labelError,
+			"labelFileFormat": fulfillment.labelFileFormat,
+			"labelFiles": fulfillment.labelFiles,
+			"labelRefund": fulfillment.labelRefund,
+			"labelUrl": fulfillment.labelUrl,
+			"manifestId": fulfillment.manifestId,
+			"muteError": fulfillment.muteError,
+			"packed": fulfillment.packed,
+			"packedAt": fulfillment.packedAt,
+			"picked": fulfillment.picked,
+			"pickedAt": fulfillment.pickedAt,
+			"price": fulfillment.price,
+			"provider": fulfillment.provider,
+			"quoteId": fulfillment.quoteId,
+			"reference": fulfillment.reference,
+			"requiresShippingApproval": fulfillment.requiresShippingApproval,
+			"saleId": fulfillment.saleId,
+			"serviceLevel": fulfillment.serviceLevel,
+			"serviceLevelTerms": fulfillment.serviceLevelTerms,
+			"shippedAt": fulfillment.shippedAt,
+			"shippoLabelIds": fulfillment.shippoLabelIds,
+			"shipstationId": fulfillment.shipstationId,
+			"status": fulfillment.status,
+			"tags": fulfillment.tags,
+			"trackingCompany": fulfillment.trackingCompany,
+			"trackingNumber": fulfillment.trackingNumber,
+			"trackingStatus": fulfillment.trackingStatus,
+			"trackingStatusDate": fulfillment.trackingStatusDate,
+			"trackingStatusDetails": fulfillment.trackingStatusDetails,
+			"trackingUrl": fulfillment.trackingUrl,
+			"updatedAt": fulfillment.updatedAt,
+			"userId": fulfillment.userId,
+			"xeroId": fulfillment.xeroId
         }
 	};
 
-	var response = await fetch(epCCFulfillmentssUrl, { method: 'POST', headers: headers, body: JSON.stringify(newFulfillmentData) });
+	var response = await fetch(epCCFulfillmentsUrl, { method: 'POST', headers: headers, body: JSON.stringify(newFulfillmentData) });
 
 	if (response.status != 201) {
 		var result = await response.json();
@@ -72,11 +124,99 @@ const postFulfillment = async function (token, fulfillment) {
 	}
 
 	var newFulfillment = await response.json();
+	
+	if (newFulfillment) {
+		// Create the fulfillment's items.
+		for (var item of fulfillment.fulfillmentItems) {
+			var newItem = await postFulfillmentItem(token, item);
+			if (newItem) {
+				await postFulfillmentItemRelationship (token, newFulfillment.data, newItem);
+			}
+		}
+	}
 
 	return newFulfillment.data;
 }
 
-// Create fulfillment/container relationship.
+/**
+ * Creates a fulfillment item.
+*/
+const postFulfillmentItem = async function (token, fulfillmentItem) {
+	var headers = epccHeaders.getHeaders('Bearer ' + token);
+
+	var newFulfillmentItemData = {
+		"data": {
+			"type": "entry",
+			"fulfillmentItemId": fulfillmentItem.id,
+			"createdAt": fulfillmentItem.createdAt,
+			"externalId": fulfillmentItem.externalId,
+			"fulfillmentId": fulfillmentItem.fulfillmentId,
+			"packed": fulfillmentItem.packed,
+			"picked": fulfillmentItem.picked,
+			"purchaseItemId": fulfillmentItem.purchaseItemId,
+			"quantity": fulfillmentItem.quantity,
+			"saleItemId": fulfillmentItem.saleItemId,
+			"shipstationId": fulfillmentItem.shipstationId,
+			"skipInventoryChange": fulfillmentItem.skipInventoryChange,
+			"updatedAt": fulfillmentItem.updatedAt,
+			"warehouseId": fulfillmentItem.warehouseId,
+			"xeroId": fulfillmentItem.xeroId
+        }
+	};
+
+	var response = await fetch(epCCFulfillmentItemsUrl, { method: 'POST', headers: headers, body: JSON.stringify(newFulfillmentItemData) });
+
+	if (response.status != 201) {
+		var result = await response.json();
+
+		console.error('Error creating fulfillment:  ' + JSON.stringify(result));
+
+		const err = new Error(JSON.stringify(result));
+		err.code = response.status;
+		throw err;
+	}
+
+	var newFulfillmentItem = await response.json();
+	
+	// TODO:  Determine corresponding EP CC order items and generate relationships.
+
+	return newFulfillmentItem.data;
+}
+
+
+/**
+ * Creates a fulfillment/item relationship.
+*/
+const postFulfillmentItemRelationship = async function (token, fulfillment, fulfillmentItem) {
+	var headers = epccHeaders.getHeaders('Bearer ' + token);
+
+	var relationship = {
+		"data": [
+			{
+				"type": "fulfillmentItems",
+				"id": fulfillmentItem.id
+			}
+		]
+	};
+
+	var containerUri = epCCFulfillmentsUrl + '/' + fulfillment.id + '/relationships/fulfillmentItems';
+
+	response = await fetch(containerUri, { method: 'POST', headers: headers, body: JSON.stringify(relationship) });
+
+	if (response.status != 201) {
+		var result = await response.json();
+
+		console.error('Error creating fulfillment/item relationship:  ' + JSON.stringify(result));
+
+		const err = new Error(JSON.stringify(result));
+		err.code = response.status;
+		throw err;
+	}
+}
+
+/**
+ * Creates a fulfillment/container relationship.
+*/
 const postFulfillmentContainerRelationship = async function (token, fulfillment, container) {
 	var headers = epccHeaders.getHeaders('Bearer ' + token);
 
